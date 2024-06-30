4034eb278d08ded0ca9912bf53016749c7edd50e
richardfernand0 <mail@richardfernando.com>
2024-06-30 16:46:44 +0800

    Upload page

--- FrontEnd/src/components/Upload.jsx
+++ FrontEnd/src/components/Upload.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import "../styles/home.css";
+import "../styles/upload.css";
 import { Navbar } from "./Navbar";
 import { Footer } from "./Footer";
 import bannerimg from "../imgs/bannerimg.png";
@@ -75,6 +75,7 @@ export class Upload extends React.Component {
                 <span style={{ color: "#449AF7" }}>Wala</span>
               </h2>
               <p className="homebannertext">Upload Your Icons Here</p>
+              <p className="uploadtext">Unleash Your Creativity - Share Your Icons & Animations (or) Upload Your Awesome Icons & Animations</p>
 
               <div className="homebannersearch-div">
                 <div className="searchdiv">
@@ -87,68 +88,13 @@ export class Upload extends React.Component {
                     onChange={(e) => this.onChange(e)}
                   />
 
-                <button onClick={this.onUpload}>Upload Now</button>
+                <button onClick={this.onUpload} class="button-59" role="button">Upload Now</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="popular-category-section">
-          <div className="popularcategory-div">
-            <div className="mostpopularcategory">
-              Most Popular Categories{" "}
-              <svg
-                fill="none"
-                viewBox="0 0 52 31"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M49 3L28.5556 23L18.3333 13L3 28M49 3H33.6667M49 3V18"
-                  stroke="#2E90EB"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="5"
-                />
-              </svg>
-            </div>
-
-            <div className="viewall-div">
-              View All{" "}
-              <svg
-                fill="none"
-                viewBox="0 0 41 40"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="m18.444 28 8.2223-8m0 0-8.2223-8m8.2223 8h-24.667m3.1153 10c3.3187 4.8239 8.9705 8 15.385 8 10.217 0 18.5-8.0589 18.5-18 0-9.9412-8.2827-18-18.5-18-6.4142 0-12.066 3.176-15.385 8"
-                  stroke="#2E90EB"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="4"
-                />
-              </svg>
-            </div>
-          </div>
-
-          <div className="categories-div">
-            <div className="categoryitem categoryitem1">
-              <img src={threedicon} alt="" className="icon-img" />
-              <p>3D Illustrations</p>
-            </div>
-            <div className="categoryitem categoryitem2">
-              <img src={vectoricon} alt="" className="icon-img" />
-              <p>Vector Icons</p>
-            </div>
-            <div className="categoryitem categoryitem3">
-              <img src={lotteicon} alt="" className="icon-img" />
-              <p>Lotte Animations</p>
-            </div>
-            <div className="categoryitem categoryitem4">
-              <img src={freeasseticon} alt="" className="icon-img" />
-              <p>Free Assets</p>
-            </div>
-          </div>
-        </div>
+      
 
         <Footer></Footer>
       </Fragment>
